0ad9a708271e093bfe2240fafd050cab4968d88e
iamsydniee <sydniemcdonald009@gmail.com>
2016-11-17 20:37:05 -0800

    added food
    working on fixing food positions, overlay, and adding game score and
    stuff

--- project6.js
+++ project6.js
@@ -10,3 +10,16 @@ document.getElementById("tiana").setAttribute("x", tianaX)
 document.getElementById("tiana").setAttribute("x", tianaX)
  }
 })
+
+var tianaY= 87;
+document.addEventListener("keydown", function(e) {
+ if(e.keyCode == 37){
+   tianaY -=15;
+document.getElementById("tiana").setAttribute("y", tianaY)
+
+ }
+ else if(e.keyCode == 39){
+       tianaY +=15;
+document.getElementById("tiana").setAttribute("y", tianaY)
+ }
+})
