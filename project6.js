6b0431585f697b7bf0ec194d7f818344c26d9329
iamsydniee <sydniemcdonald009@gmail.com>
2016-11-18 09:18:51 -0800

    overlap
    I trieddddd

--- project6.js
+++ project6.js
@@ -1,5 +1,9 @@
 var tianaX = 250;
 var tianaY = 250;
+var player = tiana;
+
+var foodCount;
+var minusHealth;
 
 document.addEventListener("keydown", function(e) {
  if(e.keyCode == 37){
@@ -13,7 +17,6 @@ document.getElementById("tiana").setAttribute("x", tianaX)
  }
 })
 
-
 document.addEventListener("keydown", function(e) {
  if(e.keyCode == 40){
    tianaY +=15;
@@ -24,4 +27,23 @@ document.getElementById("tiana").setAttribute("y", tianaY)
        tianaY -=15;
 document.getElementById("tiana").setAttribute("y", tianaY)
  }
+})
+
+//////////OverLap//////
+document.addEventListener("keydown" ,function (e) {
+  var tiana = document.getElementById.attribute("tiana")
+  var food = document.getElementById.attribute("food")
+
+tianaX = Number(tiana.getAttribute("x"))
+tianaY = Number(tiana.getAttribute("y"))
+
+foodx = Number(food.getAttribute("x"))
+foody = Number(food.getAttribute("y"))
+
+  if (tianax  > foodx && tianax < foodx + width)
+  {
+
+    food.setAttribute("begin", "food.end")
+
+  }
 })
