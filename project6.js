88ef2f2323cfd84965e3d9522ad8c1dd610ea510
iamsydniee <sydniemcdonald009@gmail.com>
2016-11-17 20:41:37 -0800

    fixed up and down player  movement
    :100:

--- project6.js
+++ project6.js
@@ -1,4 +1,6 @@
 var tianaX = 250;
+var tianaY = 250;
+
 document.addEventListener("keydown", function(e) {
  if(e.keyCode == 37){
    tianaX -=15;
@@ -11,15 +13,15 @@ document.getElementById("tiana").setAttribute("x", tianaX)
  }
 })
 
-var tianaY= 87;
+
 document.addEventListener("keydown", function(e) {
- if(e.keyCode == 37){
-   tianaY -=15;
+ if(e.keyCode == 40){
+   tianaY +=15;
 document.getElementById("tiana").setAttribute("y", tianaY)
 
  }
- else if(e.keyCode == 39){
-       tianaY +=15;
+ else if(e.keyCode == 38){
+       tianaY -=15;
 document.getElementById("tiana").setAttribute("y", tianaY)
  }
 })
